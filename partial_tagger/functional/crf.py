d5b04c84ec5a106a86f9bac88b88496ea37d0629
yasufumi <yasufumi.taniguchi@gmail.com>
2022-02-09 14:43:54 +0900

    Add crf.constrained_decode

--- partial_tagger/functional/crf.py
+++ partial_tagger/functional/crf.py
@@ -121,9 +121,8 @@ def decode(log_potentials: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         representing the maximum log probability. The second tensor is
         a [batch_size, sequence_length] integer tensor representing the tag sequence.
     """
-    with torch.enable_grad():
-        max_score = amax(log_potentials)
-        log_Z = forward_algorithm(log_potentials)
+    max_score = amax(log_potentials)
+    log_Z = forward_algorithm(log_potentials)
 
     (tag_matrix,) = torch.autograd.grad(
         max_score.sum(), log_potentials, allow_unused=True
@@ -137,6 +136,63 @@ def decode(log_potentials: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
     tag_indices = tag_bitmap.argmax(dim=-1)
 
     return max_score - log_Z, tag_indices
+
+
+def constrained_decode(
+    log_potentials: torch.Tensor,
+    mask: torch.Tensor,
+    start_constraints: torch.Tensor,
+    end_constraints: torch.Tensor,
+    transition_constraints: torch.Tensor,
+    padding_index: Optional[int] = -1,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """Computes the tag sequence gives the maximum probability for log potentials
+    with start/end/transition constraints.
+
+    Args:
+        log_potentials: A [batch_size, sequence_length - 1, num_tags, num_tags]
+        float tensor.
+        mask: A [batch_size, sequence_length] boolean tensor.
+        start_constraints: A [num_tags] boolean tensor.
+        end_constraints: A [num_tags] boolean tensor.
+        transition_constraints: A [num_tags, num_tags] boolean tensor.
+        padding_index: An integer for padded elements.
+
+    Returns:
+        A tuple of tensors. The first tensor is a [batch_size] float tensor
+        representing the maximum log probability. The second tensor is
+        a [batch_size, sequence_length] integer tensor representing the tag sequence.
+    """
+    # Apply end constraints
+    end_constraints = (
+        torch.arange(log_potentials.size(1), device=log_potentials.device)[None].lt(
+            mask.sum(dim=-1).sub(2)[..., None]
+        )
+        ^ (~mask[:, 1:])
+    )[..., None, None] | end_constraints[None, None, None]
+    constrained_log_potentials = log_potentials * end_constraints + NINF * (
+        ~end_constraints
+    )
+
+    # Apply start constraints
+    start_constraints = start_constraints[None, :, None]
+    constrained_log_potentials[:, 0] = constrained_log_potentials[
+        :, 0
+    ] * start_constraints + NINF * (~start_constraints)
+
+    # Apply transition constraints
+    transition_constraints = transition_constraints[None, None] | (
+        ~mask[:, 1:, None, None]
+    )
+    constrained_log_potentials = (
+        constrained_log_potentials * transition_constraints
+        + NINF * (~transition_constraints)
+    )
+
+    max_log_probability, tag_indices = decode(constrained_log_potentials)
+
+    tag_indices = tag_indices * mask + padding_index * (~mask)
+    return max_log_probability, tag_indices
 
 
 def sequence_score(
