4b3a72b660c2e548cf32d29187cc5f8c4abb7e50
yasufumi <yasufumi.taniguchi@gmail.com>
2022-02-21 14:17:13 +0900

    Fix helpers.compute_best_tag_indices_by_brute_force

--- tests/helpers.py
+++ tests/helpers.py
@@ -40,17 +40,18 @@ def compute_best_tag_indices_by_brute_force(
     log_potentials: torch.Tensor,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     batch_size, sequence_length, num_tags, _ = log_potentials.size()
-    best_tag_indices = torch.tensor(
-        [[-1] * (sequence_length + 1) for _ in range(batch_size)]
-    )
+    best_tag_indices = torch.tensor([[-1] * sequence_length for _ in range(batch_size)])
     max_scores = torch.tensor([NINF] * batch_size)
     for b in range(batch_size):
         max_score = torch.tensor(NINF)
-        for tag_indices in iterate_possible_tag_indices(sequence_length + 1, num_tags):
-            tag_indices_score = torch.tensor(0.0)
-            for i, (j, k) in enumerate(zip(tag_indices[:-1], tag_indices[1:])):
+        for tag_indices in iterate_possible_tag_indices(sequence_length, num_tags):
+            tag_indices_score = (
+                log_potentials[b, 0, tag_indices[0], tag_indices[0]].detach().clone()
+            )
+            for i, (j, k) in enumerate(zip(tag_indices[:-1], tag_indices[1:]), 1):
                 tag_indices_score += log_potentials[b, i, j, k]
             if tag_indices_score.gt(max_score):
+                # Ignore the dummy initial state
                 best_tag_indices[b] = torch.tensor(tag_indices)
                 max_score = tag_indices_score
         max_scores[b] = max_score
