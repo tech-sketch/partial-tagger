288531ca3fd602dff7a916f9d093bc264ad1dc83
yasufumi <yasufumi.taniguchi@gmail.com>
2022-02-21 13:42:07 +0900

    Fix the test_data_for_shape_check fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ def test_data_for_shape_check() -> tuple:
     sequence_length = 20
     num_tags = 5
     logits = torch.randn(batch_size, sequence_length, num_tags)
-    log_potentials = torch.randn(batch_size, sequence_length - 1, num_tags, num_tags)
+    log_potentials = torch.randn(batch_size, sequence_length, num_tags, num_tags)
     y = torch.randint(0, num_tags, (batch_size, sequence_length))
     tag_bitmap = torch.nn.functional.one_hot(y, num_tags).bool()
     return (batch_size, sequence_length, num_tags), logits, log_potentials, tag_bitmap
