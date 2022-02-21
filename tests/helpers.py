736ea94b958c1b53bb7802ef0af4a34f7a7a00fb
yasufumi <yasufumi.taniguchi@gmail.com>
2022-02-21 14:21:49 +0900

    Fix helpers.check_sequence_score_mask

--- tests/helpers.py
+++ tests/helpers.py
@@ -96,7 +96,16 @@ def check_sequence_score_mask(
     for b, real_sequence_length in enumerate(lengths):
         # only (i, j) is True, otherwise False
         tags = tag_indices[b, :real_sequence_length]
-        for pos, (i, j) in enumerate(zip(tags[:-1], tags[1:])):
+        if not used_mask[b, 0, tags[0], tags[0]]:
+            return False
+        for x in range(num_tags):
+            for y in range(num_tags):
+                if x == tags[0] and y == tags[0]:
+                    continue
+                if used_mask[b, 0, x, y]:
+                    return False
+
+        for pos, (i, j) in enumerate(zip(tags[:-1], tags[1:]), 1):
             if not used_mask[b, pos, i, j]:
                 return False
             for x in range(num_tags):
