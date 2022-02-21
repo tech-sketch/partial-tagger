9ff56f361ec232132e837ec2d2954353dbf4c3ed
yasufumi <yasufumi.taniguchi@gmail.com>
2022-02-21 12:08:45 +0900

    Fix a test case

--- tests/layers/test_crf.py
+++ tests/layers/test_crf.py
@@ -19,7 +19,7 @@ def test_crf_forward_returns_correct_shape(test_data_for_shape_check: tuple) -> 
     model = CRF(num_tags)
 
     assert model(logits).size() == torch.Size(
-        [batch_size, sequence_length - 1, num_tags, num_tags]
+        [batch_size, sequence_length, num_tags, num_tags]
     )
 
 
