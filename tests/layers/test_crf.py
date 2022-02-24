b82aa8e3980cbabe55148761271f8836d11e0978
yasufumi <yasufumi.taniguchi@gmail.com>
2022-02-24 11:40:57 +0900

    Add a test case for crf.ConstrainedDecoder

--- tests/layers/test_crf.py
+++ tests/layers/test_crf.py
@@ -3,7 +3,7 @@ import torch
 
 from partial_tagger.functional import crf
 from partial_tagger.layers import CRF
-from partial_tagger.layers.crf import Decoder
+from partial_tagger.layers.crf import ConstrainedDecoder, Decoder
 from tests import helpers
 
 
@@ -40,6 +40,22 @@ def test_decoder_returns_correct_shape(test_data_for_shape_check: tuple) -> None
 
     model = CRF(num_tags)
     decoder = Decoder()
+
+    max_probabilities, tag_indices = decoder(model(logits))
+
+    assert max_probabilities.size() == torch.Size([batch_size])
+    assert tag_indices.size() == torch.Size([batch_size, sequence_length])
+
+
+def test_constrained_decoder_returns_correct_shape(
+    test_data_for_shape_check: tuple,
+) -> None:
+    (batch_size, sequence_length, num_tags), logits, _, _ = test_data_for_shape_check
+
+    model = CRF(num_tags)
+    decoder = ConstrainedDecoder(
+        [True] * num_tags, [True] * num_tags, [[True] * num_tags] * num_tags
+    )
 
     max_probabilities, tag_indices = decoder(model(logits))
 
