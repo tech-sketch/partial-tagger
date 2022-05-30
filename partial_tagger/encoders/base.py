6766e25e787b15945a42fd8f5cdaaee06b75a1ae
yasufumi <yasufumi.taniguchi@gmail.com>
2022-05-30 15:56:04 +0900

    Add BaseCRFEncoder

---
+++ partial_tagger/encoders/base.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+import torch
+from torch.nn import Module
+
+from ..crf.nn import CRF
+
+
+class BaseCRFEncoder(ABC, Module):
+    def __init__(self, num_tags: int) -> None:
+        super(BaseCRFEncoder, self).__init__()
+
+        self.num_tags = num_tags
+        self.crf = CRF(num_tags)
+
+    @abstractmethod
+    def forward(
+        self, embeddings: torch.Tensor, mask: Optional[torch.Tensor]
+    ) -> torch.Tensor:
+        pass
